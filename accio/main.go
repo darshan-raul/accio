3cd251183e9cc201a656512cc56bc3dd561e947d
Darshan Raul <darshan.pwds@gmail.com>
2024-03-12 15:29:02 +0000

    feat: adds bootstrap to the html and basic htmx behaviour

--- accio/main.go
+++ accio/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Server struct{
@@ -32,7 +33,24 @@ func main(){
 
 		tmpl.Execute(w,servers)
 	}
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		name := r.PostFormValue("name")
+		servertype := r.PostFormValue("type")
+		fmt.Println(name)
+		fmt.Println(servertype)
+		//htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", name, type)
+		//tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "server-list-element", Server{Name: name, Type: servertype})
+	}
 
+	h3 := func(w http.ResponseWriter, r *http.Request){
+		fmt.Println("creating server")
+		fmt.Print(r.Header)
+	}
 	http.HandleFunc("/",h1)
+	http.HandleFunc("/add-server/", h2)
+	http.HandleFunc("/create-server/", h3)
 	log.Fatal(http.ListenAndServe(":8000",nil))
 }
